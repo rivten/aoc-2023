997149e47d7c983fad47614344b7b2efb56ad90b
rivten <rivten.grey@gmail.com>
2023-12-01 09:01:59 +0100

    rivten - day 01 - part 2

--- 01/rivten/Day01.java
+++ 01/rivten/Day01.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.stream.Collectors;
 
-static int findCalibration(String line) {
+static int findCalibrationPart1(String line) {
     var numbers = line
         .chars()
         .filter(
@@ -14,11 +14,79 @@ static int findCalibration(String line) {
     return numbers.get(0) * 10 + numbers.get(numbers.size() - 1);
 }
 
+static Integer getNumberAtIndex(String line, int i) {
+    if (line.charAt(i) >= '0' && line.charAt(i) <= '9') {
+        return line.charAt(i) - '0';
+    }
+
+    if (i + 3 > line.length()) {
+        return null;
+    }
+
+    var sub3 = line.substring(i, i + 3);
+    if (sub3.equals("one")) {
+        return 1;
+    }
+    if (sub3.equals("two")) {
+        return 2;
+    }
+    if (sub3.equals("six")) {
+        return 6;
+    }
+
+    if (i + 4 > line.length()) {
+        return null;
+    }
+
+    var sub4 = line.substring(i, i + 4);
+    if (sub4.equals("four")) {
+        return 4;
+    }
+    if (sub4.equals("five")) {
+        return 5;
+    }
+    if (sub4.equals("nine")) {
+        return 9;
+    }
+
+    if (i + 5 > line.length()) {
+        return null;
+    }
+
+    var sub5 = line.substring(i, i + 5);
+    if (sub5.equals("three")) {
+        return 3;
+    }
+    if (sub5.equals("seven")) {
+        return 7;
+    }
+    if (sub5.equals("eight")) {
+        return 8;
+    }
+
+    return null;
+}
+
+static int findCalibrationPart2(String line) {
+    int firstNumber = -1;
+    int lastNumber = -1;
+    for (int i = 0; i < line.length(); i++) {
+        Integer number = getNumberAtIndex(line, i);
+        if (number != null) {
+            if (firstNumber == -1) {
+                firstNumber = number;
+            }
+            lastNumber = number;
+        }
+    }
+    return 10 * firstNumber + lastNumber;
+}
+
 void main() {
     var bi = new BufferedReader(new InputStreamReader(System.in));
     var calibration = bi
         .lines()
-        .mapToInt(l -> findCalibration(l))
+        .mapToInt(l -> findCalibrationPart2(l))
         .sum();
     System.out.println(calibration);
 }
