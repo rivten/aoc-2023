c38ecfdebf20eb357cee9937ebc990d8896fa7e8
rivten <rivten.grey@gmail.com>
2023-12-04 10:29:25 +0100

    rivten - day 04 - part 2

--- 04/rivten/Day04.java
+++ 04/rivten/Day04.java
@@ -2,8 +2,10 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Set;
 import java.util.HashSet;
+import java.util.HashMap;
 import java.util.Arrays;
 import java.util.stream.Collectors;
+import java.util.function.Function;
 
 class Day04 {
 
@@ -24,23 +26,43 @@ class Day04 {
             return new Card(parseCards(winningCards), parseCards(youCards));
         }
 
-        private int points() {
+        private int matchingCount() {
             var common = new HashSet<Integer>(winning);
             common.retainAll(you);
-            if (common.size() == 0) {
+            return common.size();
+        }
+
+        private int points() {
+            var matchingCount = matchingCount();
+            if (matchingCount == 0) {
                 return 0;
             } else {
-                return 1 << (common.size() - 1);
+                return 1 << (matchingCount - 1);
             }
         }
     }
 
     public static void main(String[] args) {
         var bi = new BufferedReader(new InputStreamReader(System.in));
-        var sol = bi.lines()
+        var cards = bi.lines()
             .map(Card::parse)
-            .mapToInt(Card::points)
-            .sum();
-        System.out.println(sol);
+            .toList();
+        var cardCount = new HashMap<Integer, Integer>();
+        for (int cardIndex = 0; cardIndex < cards.size(); ++cardIndex) {
+            cardCount.put(cardIndex + 1, 1);
+        }
+        int cardIndex = 1;
+        for (var card: cards) {
+            var matchingCount = card.matchingCount();
+            var currentCardCount = cardCount.get(cardIndex);
+            for (int i = 0; i < matchingCount; ++i) {
+                var cardIndexToAdd = cardIndex + i + 1;
+                var oldCardCount = cardCount.get(cardIndexToAdd);
+                var newCardCount = oldCardCount + currentCardCount;
+                cardCount.put(cardIndexToAdd, newCardCount);
+            }
+            cardIndex++;
+        }
+        System.out.println(cardCount.values().stream().reduce(0, (acc, v) -> acc + v));
     }
 }
