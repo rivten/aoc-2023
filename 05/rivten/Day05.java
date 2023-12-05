d046cc373fb80c94861247bcb483fe0fa902e844
rivten <rivten@riseup.net>
2023-12-05 10:17:35 +0100

    rivten - day 05 - part 2
    with all the debug logging just so you can see how desperate i was

--- 05/rivten/Day05.java
+++ 05/rivten/Day05.java
@@ -3,23 +3,147 @@ import java.io.InputStreamReader;
 import java.util.stream.Collectors;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Optional;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
 
 class Day05 {
 
-    private record Range(long src, long len, long dst) {}
+    // end is non-inclusive
+    private record Interval(long start, long end) {
+        private boolean isIn(long in) {
+            return in >= start && in < end;
+        }
+
+        private Interval intersection(Interval other) {
+            if (this.start > other.start) {
+                return other.intersection(this);
+            }
+
+            if (this.end < other.start) {
+                // empty intersection
+                return new Interval(0, 0);
+            } else if (this.end < other.end) {
+                return new Interval(other.start, this.end);
+            } else {
+                return other;
+            }
+        }
+
+        private boolean empty() {
+            return start == end;
+        }
+
+        private List<Interval> sub(Interval other) {
+            var result = new ArrayList<Interval>();
+            var intersection = this.intersection(other);
+            if (intersection.empty()) {
+                result.add(this);
+            } else {
+                //if (this.start == 46 && other.start == 56) {
+                //    System.out.println(this);
+                //    System.out.println(other);
+                //    System.out.println(intersection);
+                //}
+                if (intersection.start == this.start) {
+                    if (intersection.end < this.end) {
+                        result.add(new Interval(intersection.end, this.end));
+                    } else {
+                        // substract all, do nothing
+                    }
+                } else { // intersection.start > this.start
+                    if (intersection.end < this.end) {
+                        result.add(new Interval(this.start, intersection.start));
+                        result.add(new Interval(intersection.end, this.end));
+                    } else {
+                        result.add(new Interval(this.start, intersection.start));
+                    }
+                }
+            }
+            return result;
+        }
+    }
+
+    private record Range(Interval interval, long dst) {
+        private Optional<Long> map(long in) {
+            if (interval.isIn(in)) {
+                return Optional.of(in - interval.start + dst);
+            } else {
+                return Optional.empty();
+            }
+        }
+    }
 
     private record Mapping(List<Range> ranges) {
+        private List<Interval> map(Interval interval) {
+            //if (interval.start == 46 && this.ranges.get(0).interval.start == 56) {
+            //    System.out.println(">>>");
+            //    System.out.println(this);
+            //    System.out.println(interval);
+            //}
+            var intervals = new ArrayList<Interval>();
+
+            var remainingParts = new ArrayList<Interval>();
+            remainingParts.add(interval);
+
+            for (var range: ranges) {
+                //if (interval.start == 46 && this.ranges.get(0).interval.start == 56) {
+                //    System.out.println("$$");
+                //    System.out.println(ranges);
+                //}
+                var intersection = interval.intersection(range.interval);
+                if (!intersection.empty()) {
+                    //if (interval.start == 46 && this.ranges.get(0).interval.start == 56) {
+                    //    System.out.println("found intersection");
+                    //    System.out.println(intersection);
+                    //    System.out.println(interval);
+                    //    System.out.println(range.interval);
+                    //}
+                    intervals.add(new Interval(
+                        intersection.start - range.interval.start + range.dst,
+                        intersection.end - range.interval.start + range.dst
+                    ));
+                    var nextRemainingParts = new ArrayList<Interval>();
+                    for (var remainingPart: remainingParts) {
+                        var substraction = remainingPart.sub(range.interval);
+                        //if (interval.start == 46 && this.ranges.get(0).interval.start == 56) {
+                        //    System.out.println("bb");
+                        //    System.out.println(remainingPart);
+                        //    System.out.println(range.interval);
+                        //    System.out.println(substraction);
+                        //}
+                        for (var s: substraction) {
+                            nextRemainingParts.add(s);
+                        }
+                    }
+                    remainingParts = nextRemainingParts;
+                    //if (interval.start == 46 && this.ranges.get(0).interval.start == 56) {
+                    //    System.out.println("aa");
+                    //    System.out.println(remainingParts);
+                    //}
+                }
+            }
+
+            // add all non-mapped parts of the initial interval
+            for (var remainingPart: remainingParts) {
+                intervals.add(remainingPart);
+            }
+
+            return intervals;
+        }
+
         private long map(long in) {
             for (var range: ranges) {
-                if (in >= range.src && in < (range.src + range.len)) {
-                    return (in - range.src) + range.dst;
+                var maybeOut = range.map(in);
+                if (maybeOut.isPresent()) {
+                    return maybeOut.get();
                 }
             }
             return in;
         }
     }
 
-    private record Almanac(List<Long> seeds, Mapping[] mappings) {
+    private record Almanac(List<Interval> seeds, Mapping[] mappings) {
         private long getLoc(long seed) {
             var currentIndex = seed;
             for (var mapping: mappings) {
@@ -27,17 +151,51 @@ class Day05 {
             }
             return currentIndex;
         }
+
+        private long getSmallestLoc(Interval seed) {
+            //System.out.println(">>");
+            //System.out.println(seed);
+            var currentIntervals = new ArrayList<Interval>();
+            currentIntervals.add(seed);
+            for (var mapping: mappings) {
+                //System.out.println(">>>$");
+                //System.out.println(mapping);
+                //System.out.println(currentIntervals);
+                var nextIntervals = new ArrayList<Interval>();
+                for (var interval: currentIntervals) {
+                    for (var i: mapping.map(interval)) {
+                        nextIntervals.add(i);
+                    }
+                }
+                //System.out.println(nextIntervals);
+                currentIntervals = nextIntervals;
+            }
+            //System.out.println("$$");
+            //System.out.println(currentIntervals);
+            return currentIntervals.stream()
+                .mapToLong(Interval::start)
+                .min()
+                .getAsLong();
+        }
     }
 
     private static Almanac parse(String content) {
         var mappings = new Mapping[7];
-        var seeds = new ArrayList<Long>();
+        var seeds = new ArrayList<Interval>();
         var paragraphIndex = 0;
         for (var paragraph: content.split("\n\n")) {
             if (paragraphIndex == 0) {
+                long lastSeedSrc = 0;
+                var seedNumberIndex = 0;
                 var seedsRaw = paragraph.split(":")[1];
                 for (var seedRaw: seedsRaw.trim().split(" ")) {
-                    seeds.add(Long.parseLong(seedRaw));
+                    if (seedNumberIndex % 2 == 0) {
+                        lastSeedSrc = Long.parseLong(seedRaw);
+                    } else {
+                        var seedLen = Long.parseLong(seedRaw);
+                        seeds.add(new Interval(lastSeedSrc, lastSeedSrc + seedLen));
+                    }
+                    seedNumberIndex++;
                 }
             } else {
                 var mappingIndex = paragraphIndex - 1;
@@ -47,9 +205,10 @@ class Day05 {
                     // starting at one to skip the first line
                     var rawLine = rawLines[i];
                     var rawInts = rawLine.split(" ");
+                    var src = Long.parseLong(rawInts[1]);
+                    var len = Long.parseLong(rawInts[2]);
                     var range = new Range(
-                        Long.parseLong(rawInts[1]),
-                        Long.parseLong(rawInts[2]),
+                        new Interval(src, src + len),
                         Long.parseLong(rawInts[0])
                     );
                     ranges.add(range);
@@ -68,7 +227,7 @@ class Day05 {
 
         var almanac = parse(content);
         var sol = almanac.seeds.stream()
-            .mapToLong(almanac::getLoc)
+            .mapToLong(almanac::getSmallestLoc)
             .min()
             .getAsLong();
         System.out.println(sol);
