75a0b8365e58f30af2705bb663f7aed750076933
rivten <rivten@riseup.net>
2023-12-06 10:51:23 +0100

    rivten - day 06 - part 2

--- 06/rivten/Day06.java
+++ 06/rivten/Day06.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 
 class Day06 {
 
-    private record Race(int t, int d) {
-        private int solveCount() {
+    private record Race(long t, long d) {
+        private long solveCount() {
             // let's call tb the time you press the button.
             // then we have the equation
             // (t - tb) * tb <= d
@@ -33,8 +33,8 @@ class Day06 {
             var sqrtDelta = Math.sqrt(delta);
             var s0 = (t - sqrtDelta) / 2.0;
             var s1 = (t + sqrtDelta) / 2.0;
-            var start = (int)Math.ceil(Math.max(Math.min(epsilon + s0, t), 0.0));
-            var end = (int)Math.floor(Math.max(Math.min(-epsilon + s1, t), 0.0));
+            var start = (long)Math.ceil(Math.max(Math.min(epsilon + s0, t), 0.0));
+            var end = (long)Math.floor(Math.max(Math.min(-epsilon + s1, t), 0.0));
             return end + 1 - start;
         }
     }
@@ -42,8 +42,8 @@ class Day06 {
     private static Race[] parse() {
         var bi = new BufferedReader(new InputStreamReader(System.in));
         var lines = bi.lines().toList();
-        var times = Arrays.stream(lines.get(0).split(":")[1].trim().split(" ")).filter(s -> s.length() != 0).map(Integer::parseInt).toList();
-        var distances = Arrays.stream(lines.get(1).split(":")[1].trim().split(" ")).filter(s -> s.length() != 0).map(Integer::parseInt).toList();
+        var times = Arrays.stream(lines.get(0).split(":")[1].trim().split(" ")).filter(s -> s.length() != 0).map(Long::parseLong).toList();
+        var distances = Arrays.stream(lines.get(1).split(":")[1].trim().split(" ")).filter(s -> s.length() != 0).map(Long::parseLong).toList();
         var races = new Race[times.size()];
         for (int i = 0; i < times.size(); ++i) {
             races[i] = new Race(times.get(i), distances.get(i));
@@ -52,11 +52,21 @@ class Day06 {
         return races;
     }
 
+    private static Race parseRace() {
+        var bi = new BufferedReader(new InputStreamReader(System.in));
+        var lines = bi.lines().toList();
+        var t = Long.parseLong(lines.get(0).split(":")[1].replaceAll("\\s", ""));
+        var d = Long.parseLong(lines.get(1).split(":")[1].replaceAll("\\s", ""));
+        return new Race(t, d);
+    }
+
     public static void main(String[] args) {
-        var races = parse();
-        var sol = Arrays.stream(races)
-            .mapToInt(Race::solveCount)
-            .reduce(1, (a, b) -> a * b);
+        //var races = parse();
+        //var sol = Arrays.stream(races)
+        //    .mapToInt(Race::solveCount)
+        //    .reduce(1, (a, b) -> a * b);
+        var race = parseRace();
+        var sol = race.solveCount();
         System.out.println(sol);
     }
 }
